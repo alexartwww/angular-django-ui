db69fb8f8505b83bea8ab7788d3dc934954c3113
Artem Aleksashkin <artem@aleksashkin.com>
2018-08-23 02:34:13 +0300

    night

---
+++ backend/api/serializers.py
@@ -0,0 +1,8 @@
+from api.models import Hero
+from rest_framework import serializers
+
+
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        fields = ('id', 'name')
--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
     'rest_framework',
 ]
 
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
